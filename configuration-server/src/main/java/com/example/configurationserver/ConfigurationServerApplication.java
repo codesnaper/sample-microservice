4117e5e434bde2d56f264e0bc546ecbbbb23fecd
SHUBHAM KHANDELWAL <shubham.khandelwal019@gmail.com>
2023-07-23 13:29:16 +0200

    Add TLS Communication on config and discovery server Add client on config

--- configuration-server/src/main/java/com/example/configurationserver/ConfigurationServerApplication.java
+++ configuration-server/src/main/java/com/example/configurationserver/ConfigurationServerApplication.java
@@ -1,17 +1,15 @@
 package com.example.configurationserver;
 
-import com.example.configurationserver.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigurationServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigurationServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServerApplication.class, args);
+    }
 
 }
--- configuration-server/src/main/java/com/example/configurationserver/config/AppConfig.java
+++ configuration-server/src/main/java/com/example/configurationserver/config/AppConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,7 +15,7 @@ public class AppConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authz -> authz.anyRequest().permitAll())
+                .authorizeHttpRequests(authz -> authz.requestMatchers("/encrypt", "/decrypt", "/actuator/*").permitAll().anyRequest().authenticated())
                 .build();
     }
 
